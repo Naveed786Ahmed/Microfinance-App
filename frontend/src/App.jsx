857bcdbb297680f859ca287531e300a8d8600976
Naveed786Ahmed <naveedsami5000@gmail.com>
2026-04-07 23:09:06 +0500

    add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import HomePage from './pages/Home.jsx'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Layout/Navbar.jsx'
 
 const App = () => {
   return (
     <>
-      
+      <div className='min-h-screen bg-slate-50'>
+        <Navbar />
+        <div className="pt-20">
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+          </Routes>
+        </div>
+      </div>
     </>
   )
 }
---
+++ frontend/src/constant/string.js
@@ -0,0 +1,7 @@
+export const STRING = {
+    SAYLANI: "Saylani",
+    SAHULAT: "Sahulat",
+    LOGIN: "Login",
+    REGISTER: "Register",
+    REGISTER_NOW: "Register Now"
+}
