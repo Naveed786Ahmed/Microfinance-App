cfdc5b4fdb080bff408dd868c8661e4558cbe24d
Naveed786Ahmed <naveedsami5000@gmail.com>
2026-04-08 23:10:36 +0500

    add hero & categories section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import HeroSec from '../components/Layout/HeroSec'
+import Categories from '../components/Layout/Categories'
 
 const HomePage = () => {
   return (
     <>
+    <HeroSec />
+    <Categories />
     </>
   )
 }
